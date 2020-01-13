414ff36c2d01aa2fb91e478e9375b546dabffc7d
DejanOs <deanhinich@gmail.com>
2020-01-13 15:57:15 +0000

    basic setup done and connected to mongo

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
 app = Flask(__name__)
-
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:0t0rin0laring0l0gija@myfirstcluster-ol1er.mongodb.net/cook_book?retryWrites=true&w=majority')
 mongo = PyMongo(app)
 
 
