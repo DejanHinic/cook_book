22fae9d97bf5a5df48ebb44ba3e8e6ad96cb70d4
DejanOs <deanhinich@gmail.com>
2020-01-21 13:34:43 +0000

    add recipe working

--- app.py
+++ app.py
@@ -16,8 +16,14 @@ def get_recipes():
                            recipes=mongo.db.recipes.find())
 
 @app.route('/add_recipe')
-def add_task():
+def add_recipe():
     return render_template('add_recipe.html')
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipe = mongo.db.recipes
+    recipe.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
 
 
 if __name__ == '__main__':
